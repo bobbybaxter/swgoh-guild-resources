efa876d6fedc40296d18fefa3221f8205c69eca5
Bob <bob.e.baxter@gmail.com>
2019-11-17 21:46:36 -0600

    Updates

---
+++ precache-manifest.0c098f253a6f3fa779c2d5d2421678d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ec4358a69a7b95753f0b22e9940c74e",
+    "url": "/swgoh-guild-resources/index.html"
+  },
+  {
+    "revision": "316ab669651bc8a9d4fa",
+    "url": "/swgoh-guild-resources/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "9ca75e7c12252ed895ab",
+    "url": "/swgoh-guild-resources/static/css/main.0c96a878.chunk.css"
+  },
+  {
+    "revision": "316ab669651bc8a9d4fa",
+    "url": "/swgoh-guild-resources/static/js/2.c5362fcc.chunk.js"
+  },
+  {
+    "revision": "9ca75e7c12252ed895ab",
+    "url": "/swgoh-guild-resources/static/js/main.e7983a45.chunk.js"
+  },
+  {
+    "revision": "106621cdbb44763104a8",
+    "url": "/swgoh-guild-resources/static/js/runtime-main.2ff03298.js"
+  }
+]);
